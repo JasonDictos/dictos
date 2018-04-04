909297e9928cf266237c5555a461e127e775cba5
Jason Dictos <jason@dictos.com>
2018-04-04 15:04:06 -0700

    Fix building on linux with automatic symbolic creation

--- config-nix.bat
+++ config-nix.bat
@@ -1,3 +1,8 @@
+git submodule update --init net
 rm -rf _build 2>/dev/null
 mkdir _build 2>/dev/null
+rm inc/dictos/core
+ln -s ../ inc/dictos/core
+rm net/inc/dictos/net
+ln -s ../ net/inc/dictos/net
 cmake -H. -B_build -DCMAKE_BUILD_TYPE=Debug
