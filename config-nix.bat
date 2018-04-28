97591ebcb160a6e1a0c84a2500bdd4382b81fb54
Jason Dictos <jason@dictos.com>
2018-04-27 21:19:10 -0700

    Remove

--- config-nix.bat
+++
@@ -1,9 +0,0 @@
-setlocal && exit /b
-git submodule update --init net
-rm -rf _build 2>/dev/null
-mkdir _build 2>/dev/null
-rm inc/dictos/core
-ln -s ../ inc/dictos/core
-rm net/inc/dictos/net
-ln -s ../ net/inc/dictos/net
-cmake -H. -B_build -DCMAKE_BUILD_TYPE=Debug
